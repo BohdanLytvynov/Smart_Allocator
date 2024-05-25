5fc8b05a5008f748c5f2aa43c474aa22b932ef03
BohdanLytvynov <blitvinov19@gmail.com>
2024-05-25 10:33:40 +0300

    Smart allocator object ready for use

--- Smart_Allocator/Smart_Allocator.cpp
+++ Smart_Allocator/Smart_Allocator.cpp
@@ -5,49 +5,48 @@
 #include <crtdbg.h>
 #include"..\smart_alloc_dll\smart_alloc.h"
 
-namespace smart_allocator
+
+allocator::smart_allocator<int> Returning()
 {
-	allocator::smart_allocator<int> Returning()
+	return allocator::smart_allocator<int>(20);
+}
+
+void DisposeTest()
+{
+	allocator::smart_allocator<int> alloc(23);
+}
+
+void ExcepTest()
+{
+	try
 	{
-		return allocator::smart_allocator<int>(20);
+		allocator::smart_allocator<char> test('e');
+
+		throw std::exception("Test");
 	}
-
-	void DisposeTest()
+	catch (const std::exception&)
 	{
-		allocator::smart_allocator<int> alloc(23);
-	}
-
-	void ExcepTest()
-	{
-		try
-		{
-			allocator::smart_allocator<char> test('e');
-
-			throw std::exception("Test");
-		}
-		catch (const std::exception&)
-		{
-			return;
-		}
-	}
-
-	extern "C" int main()
-	{
-		int flag = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG);
-		flag |= _CRTDBG_LEAK_CHECK_DF;
-		_CrtSetDbgFlag(flag);
-
-		std::cout << "Memory Leak Testing!\n";
-
-		allocator::smart_allocator<int> res = Returning();
-
-		DisposeTest();
-
-		ExcepTest();
-
-		return EXIT_SUCCESS;
+		return;
 	}
 }
+
+int main()
+{
+	int flag = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG);
+	flag |= _CRTDBG_LEAK_CHECK_DF;
+	_CrtSetDbgFlag(flag);
+
+	std::cout << "Memory Leak Testing!\n";
+
+	allocator::smart_allocator<int> res = Returning();
+
+	DisposeTest();
+
+	ExcepTest();
+
+	return EXIT_SUCCESS;
+}
+
 
 
 
